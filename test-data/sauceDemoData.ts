e57f19a851e6f58c04d2ba3b4eaa8a57fcf793d1
abirami485-png <abirami485@gmail.com>
2026-07-20 19:59:03 +0530

    Create test-data/sauceDemoData.ts

---
+++ test-data/sauceDemoData.ts
@@ -0,0 +1,18 @@
+export const sauceDemoData = {
+  tc001: {
+    firstProduct: 'Sauce Labs Backpack',
+    secondProduct: 'Sauce Labs Bike Light',
+    firstName: 'John',
+    lastName: 'Doe',
+    postalCode: '12345'
+  },
+  tc002: {
+    username: 'invalid_user',
+    password: 'invalid_password'
+  },
+  tc004: {
+    firstName: '',
+    lastName: '',
+    postalCode: ''
+  }
+} as const;
