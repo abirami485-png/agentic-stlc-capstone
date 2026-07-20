8542b21417d291201de207c2304886a5a70839a5
abirami485-png <abirami485@gmail.com>
2026-07-20 20:20:08 +0530

    Create utils/selectors.ts

---
+++ utils/selectors.ts
@@ -0,0 +1,16 @@
+const productSlugMap: Record<string, string> = {
+  'Sauce Labs Backpack': 'sauce-labs-backpack',
+  'Sauce Labs Bike Light': 'sauce-labs-bike-light',
+  'Sauce Labs Bolt T-Shirt': 'sauce-labs-bolt-t-shirt',
+  'Sauce Labs Fleece Jacket': 'sauce-labs-fleece-jacket',
+  'Sauce Labs Onesie': 'sauce-labs-onesie',
+  'Test.allTheThings() T-Shirt (Red)': 'test.allthethings()-t-shirt-(red)'
+};
+
+export function productTestId(action: 'add-to-cart' | 'remove-from-cart', productName: string): string {
+  const slug = productSlugMap[productName];
+  if (!slug) {
+    throw new Error(`Unsupported product name: ${productName}`);
+  }
+  return `${action}-${slug}`;
+}
