ccbb7a809a8f6a6d06ae4ad38f33fec8f21e04d8
abirami485-png <abirami485@gmail.com>
2026-07-21 18:43:28 +0530

    Create test-data/sauce-demo-data.ts

---
+++ test-data/sauce-demo-data.ts
@@ -0,0 +1,20 @@
+export const sauceDemoUsers = {
+  standardUser: {
+    username: 'standard_user',
+    password: 'secret_sauce'
+  },
+  invalidUser: {
+    username: 'invalid_user',
+    password: 'wrong_password'
+  }
+};
+
+export const sauceDemoProducts = {
+  backpack: 'Sauce Labs Backpack'
+};
+
+export const checkoutCustomer = {
+  firstName: 'John',
+  lastName: 'Doe',
+  postalCode: '12345'
+};
