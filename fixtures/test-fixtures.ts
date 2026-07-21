d7fc31578c053ad834b984ff8185e4cbf74f9a28
abirami485-png <abirami485@gmail.com>
2026-07-21 18:44:02 +0530

    Create fixtures/test-fixtures.ts

---
+++ fixtures/test-fixtures.ts
@@ -0,0 +1,27 @@
+import { test as base } from '@playwright/test';
+import { CartPage } from '../pages/cart-page';
+import { CheckoutPage } from '../pages/checkout-page';
+import { InventoryPage } from '../pages/inventory-page';
+import { LoginPage } from '../pages/login-page';
+
+export const test = base.extend<{
+  loginPage: LoginPage;
+  inventoryPage: InventoryPage;
+  cartPage: CartPage;
+  checkoutPage: CheckoutPage;
+}>({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+  inventoryPage: async ({ page }, use) => {
+    await use(new InventoryPage(page));
+  },
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page));
+  },
+  checkoutPage: async ({ page }, use) => {
+    await use(new CheckoutPage(page));
+  }
+});
+
+export { expect } from '@playwright/test';
