ed047432673552d04ac6a7b566a03c361cac0157
abirami485-png <abirami485@gmail.com>
2026-07-21 17:06:52 +0530

    Create utils/testData.ts

---
+++ utils/testData.ts
@@ -0,0 +1,16 @@
+export const testData = {
+  validUser: {
+    username: 'standard_user',
+    password: 'secret_sauce'
+  },
+  invalidUser: {
+    username: 'invalid_user',
+    password: 'invalid_password'
+  },
+  productName: 'Sauce Labs Backpack',
+  customer: {
+    firstName: 'John',
+    lastName: 'Doe',
+    postalCode: '12345'
+  }
+};
