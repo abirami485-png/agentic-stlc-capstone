38743916f787a08e10cbaf9c9ae4a8a9f28f0010
abirami485-png <abirami485@gmail.com>
2026-07-21 18:23:35 +0530

    Create utils/testData.ts

---
+++ utils/testData.ts
@@ -0,0 +1,11 @@
+export const checkoutCustomer = {
+  firstName: 'John',
+  lastName: 'Doe',
+  postalCode: '12345'
+};
+
+export const invalidCheckoutCustomer = {
+  firstName: 'John',
+  lastName: 'Doe',
+  postalCode: ''
+};
