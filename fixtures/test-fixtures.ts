d717ab97672459c51030abb406d44e270080bb06
abirami485-png <abirami485@gmail.com>
2026-07-21 18:23:50 +0530

    Create fixtures/test-fixtures.ts

---
+++ fixtures/test-fixtures.ts
@@ -0,0 +1,40 @@
+import { test as base, expect, Page } from '@playwright/test';
+import { LoginPage } from '@pages/LoginPage';
+import { InventoryPage } from '@pages/InventoryPage';
+import { CartPage } from '@pages/CartPage';
+import { CheckoutInformationPage } from '@pages/CheckoutInformationPage';
+import { CheckoutOverviewPage } from '@pages/CheckoutOverviewPage';
+import { CheckoutCompletePage } from '@pages/CheckoutCompletePage';
+import { env } from '@utils/env';
+
+type Fixtures = {
+  loginPage: LoginPage;
+  inventoryPage: InventoryPage;
+  cartPage: CartPage;
+  checkoutInformationPage: CheckoutInformationPage;
+  checkoutOverviewPage: CheckoutOverviewPage;
+  checkoutCompletePage: CheckoutCompletePage;
+};
+
+export const test = base.extend<Fixtures>({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+  inventoryPage: async ({ page }, use) => {
+    await use(new InventoryPage(page));
+  },
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page));
+  },
+  checkoutInformationPage: async ({ page }, use) => {
+    await use(new CheckoutInformationPage(page));
+  },
+  checkoutOverviewPage: async ({ page }, use) => {
+    await use(new CheckoutOverviewPage(page));
+  },
+  checkoutCompletePage: async ({ page }, use) => {
+    await use(new CheckoutCompletePage(page));
+  }
+});
+
+export { expect, env, Page };
