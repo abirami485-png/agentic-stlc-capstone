43068529b509d3e6fd16046ab246a295678098a5
abirami485-png <abirami485@gmail.com>
2026-07-21 18:43:45 +0530

    Create pages/checkout-page.ts

---
+++ pages/checkout-page.ts
@@ -0,0 +1,41 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+export class CheckoutPage {
+  readonly page: Page;
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly postalCodeInput: Locator;
+  readonly continueButton: Locator;
+  readonly finishButton: Locator;
+  readonly completeHeader: Locator;
+  readonly errorMessage: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.firstNameInput = page.getByPlaceholder('First Name');
+    this.lastNameInput = page.getByPlaceholder('Last Name');
+    this.postalCodeInput = page.getByPlaceholder('Zip/Postal Code');
+    this.continueButton = page.getByRole('button', { name: 'Continue' });
+    this.finishButton = page.getByRole('button', { name: 'Finish' });
+    this.completeHeader = page.getByText('Thank you for your order!');
+    this.errorMessage = page.locator('[data-test="error"]');
+  }
+
+  async fillCustomerInformation(firstName: string, lastName: string, postalCode: string): Promise<void> {
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.postalCodeInput.fill(postalCode);
+  }
+
+  async continue(): Promise<void> {
+    await this.continueButton.click();
+  }
+
+  async finish(): Promise<void> {
+    await this.finishButton.click();
+  }
+
+  async expectOrderComplete(): Promise<void> {
+    await expect(this.completeHeader).toBeVisible();
+  }
+}
