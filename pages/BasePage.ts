d762ab8f9c1afd0875276f038f6bee0ccb53dae2
abirami485-png <abirami485@gmail.com>
2026-07-21 18:24:30 +0530

    Temp

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -1,5 +1,5 @@
 import { expect, Page } from '@playwright/test';
-import { env } from '@utils/env';
+import { env } from '../utils/env';
 
 export class BasePage {
   constructor(protected readonly page: Page) {}
