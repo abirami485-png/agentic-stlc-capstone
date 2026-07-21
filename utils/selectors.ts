956f3fbf65118ab04c8cdba50a61cdafa3062ecb
abirami485-png <abirami485@gmail.com>
2026-07-21 18:43:31 +0530

    Create utils/selectors.ts

---
+++ utils/selectors.ts
@@ -0,0 +1,8 @@
+export const appSelectors = {
+  inventoryItem: '.inventory_item',
+  cartItem: '.cart_item',
+  errorMessage: '[data-test="error"]',
+  cartBadge: '[data-test="shopping-cart-badge"]',
+  backpackButton: '[data-test="add-to-cart-sauce-labs-backpack"]',
+  removeBackpackButton: '[data-test="remove-sauce-labs-backpack"]'
+};
