2ec83132a4d6ca6185bf87f30cbac151174a47ce
abirami485-png <abirami485@gmail.com>
2026-07-21 18:24:45 +0530

    Temp

--- pages/CheckoutCompletePage.ts
+++ pages/CheckoutCompletePage.ts
@@ -1,5 +1,5 @@
 import { expect, Page } from '@playwright/test';
-import { BasePage } from '@pages/BasePage';
+import { BasePage } from './BasePage';
 
 export class CheckoutCompletePage extends BasePage {
   constructor(page: Page) {
