51545c1816edfe47d3877484cf40c2a81205321b
abirami485-png <abirami485@gmail.com>
2026-07-21 18:26:01 +0530

    Redirect test data

--- utils/testData.ts
+++ utils/testData.ts
@@ -1,11 +1,1 @@
-export const checkoutCustomer = {
-  firstName: 'John',
-  lastName: 'Doe',
-  postalCode: '12345'
-};
-
-export const invalidCheckoutCustomer = {
-  firstName: 'John',
-  lastName: 'Doe',
-  postalCode: ''
-};
+export { checkoutCustomer, invalidCheckoutCustomer } from '../test-data/test-data';
