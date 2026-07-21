d8516ad11a8860c5bd134a5551fdf38148ca5ec7
abirami485-png <abirami485@gmail.com>
2026-07-21 18:07:11 +0530

    Generated Playwright automation framework - Create pages/base.page.ts

---
+++ pages/base.page.ts
@@ -0,0 +1,13 @@
+import type { Page } from '@playwright/test';
+
+export class BasePage {
+  constructor(protected readonly page: Page) {}
+
+  async open(path = '/'): Promise<void> {
+    await this.page.goto(path);
+  }
+
+  async currentPath(): Promise<string> {
+    return new URL(this.page.url()).pathname;
+  }
+}
