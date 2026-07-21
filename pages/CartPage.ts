eb478257dae69f073910e13360c8b552956ce065
abirami485-png <abirami485@gmail.com>
2026-07-21 18:23:56 +0530

    Create pages/CartPage.ts

---
+++ pages/CartPage.ts
@@ -0,0 +1,23 @@
+import { expect, Page } from '@playwright/test';
+import { BasePage } from '@pages/BasePage';
+
+export class CartPage extends BasePage {
+  private readonly cartContents = this.page.locator('.cart_contents_container');
+  private readonly checkoutButton = this.page.getByRole('button', { name: 'Checkout' });
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async expectLoaded() {
+    await expect(this.cartContents).toBeVisible();
+  }
+
+  async removeFirstProduct() {
+    await this.page.getByRole('button', { name: 'Remove' }).first().click();
+  }
+
+  async checkout() {
+    await this.checkoutButton.click();
+  }
+}
